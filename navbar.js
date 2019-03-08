a5e0683978a9555c88f25afb7bcf11e2523733da
John Johnmann <jakublosko@gmail.com>
2019-03-08 19:04:18 +0100

    add navbar

---
+++ navbar.js
@@ -0,0 +1,7 @@
+let nav = '<nav class="nav"><a class="nav-link" href="index.html">Home</a>'+
+    '<a class="nav-link" href="#">Blog</a>'+
+    '<a class="nav-link" href="#">Quiz</a>'+
+    '<a class="nav-link" href="contact.html">Kontakt</a></nav>';
+
+let elem = document.querySelector(".navbar");
+elem.innerHTML = nav;
