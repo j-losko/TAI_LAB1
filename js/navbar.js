ba9b4c7acc417eb4dab183677d8cb9537bc90948
-unset <-unset>
2019-03-15 16:41:14 +0100

    add quiz

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 let nav = '<nav class="nav"><a class="nav-link" href="index.html">Home</a>'+
     '<a class="nav-link" href="#">Blog</a>'+
-    '<a class="nav-link" href="#">Quiz</a>'+
+    '<a class="nav-link" href="quiz.html">Quiz</a>'+
     '<a class="nav-link" href="contact.html">Kontakt</a></nav>';
 
 let elem = document.querySelector(".navbar");
